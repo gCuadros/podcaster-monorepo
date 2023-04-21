dade420c4f45a2293df701f1a8035dfee851c118
Gonzalo C <gondev@MacBook-Pro-de-Gonzalo.local>
2023-04-21 02:32:03 +0200

    feat(main-view): add staticProps

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,4 +1,23 @@
+import { QueryClient, dehydrate } from "@tanstack/react-query";
+import { GetStaticProps } from "next";
+
+import { podcastsKey } from "api/hooks/podcasts/usePodcasts";
+import { fetchPodcasts } from "api/hooks/podcasts/usePodcasts";
+
 import Home from "containers/Home";
+
+export const getStaticProps: GetStaticProps = async ({}) => {
+  const queryClient = new QueryClient();
+
+  queryClient.prefetchQuery(podcastsKey({}), fetchPodcasts);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  };
+};
 
 const Page = () => {
   return <Home />;
